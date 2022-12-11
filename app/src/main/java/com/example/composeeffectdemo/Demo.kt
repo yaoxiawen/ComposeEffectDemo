538a047e36d6cff71c2c4dc126b96778e4842fe2
yaoxiawen <735649296@qq.com>
2022-12-11 15:58:10 +0800

    LaunchedEffect，启动协程

--- app/src/main/java/com/example/composeeffectdemo/Demo.kt
+++ app/src/main/java/com/example/composeeffectdemo/Demo.kt
@@ -1,6 +1,35 @@
 package com.example.composeeffectdemo
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
+/**
+ * LaunchedEffect，启动协程
+ */
 @Composable
-fun Demo1() {}
+fun Demo1() {
+    var state by remember { mutableStateOf(false) }
+    val scaffoldState = rememberScaffoldState()
+    LaunchedEffect(state) {
+        scaffoldState.snackbarHostState.showSnackbar(
+            message = "click message",
+            actionLabel = "retry"
+        )
+    }
+    Scaffold(scaffoldState = scaffoldState, content = {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Button(onClick = {
+                state = !state
+            }) {
+                Text(text = "click")
+            }
+        }
+    })
+}
