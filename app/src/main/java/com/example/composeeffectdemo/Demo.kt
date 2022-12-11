d3cb124d3f138e41e6a634ffdc69192a874ae68f
yaoxiawen <735649296@qq.com>
2022-12-11 16:27:05 +0800

    rememberCoroutineScope，拿到协程作用域

--- app/src/main/java/com/example/composeeffectdemo/Demo.kt
+++ app/src/main/java/com/example/composeeffectdemo/Demo.kt
@@ -2,13 +2,13 @@ package com.example.composeeffectdemo
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
 /**
  * LaunchedEffect，启动协程
@@ -32,4 +32,43 @@ fun Demo1() {
             }
         }
     })
+}
+
+/**
+ * rememberCoroutineScope，拿到协程作用域
+ * 在可组合项外启动协程，手动控制一个或多个协程的生命周期
+ * 可组合项传入一个回调，在回调里把rememberCoroutineScope传出去就是在可组合项外启动协程
+ */
+@Composable
+fun Demo2() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "抽屉内容区域")
+            }
+        },
+        topBar = {
+            TopAppBar(title = {}, navigationIcon = {
+                IconButton(onClick = {
+                    scope.launch { scaffoldState.drawerState.open() }
+                }) {
+                    Icon(imageVector = Icons.Filled.Menu, contentDescription = "")
+                }
+            })
+        },
+        content = {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "内容区域")
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(text = { Text(text = "悬浮按钮") }, onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("click 悬浮按钮")
+                }
+            })
+        })
 }
--- app/src/main/java/com/example/composeeffectdemo/MainActivity.kt
+++ app/src/main/java/com/example/composeeffectdemo/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeEffectDemoTheme {
-                Demo1()
+                Demo2()
             }
         }
     }
